ccf676ef9209d1cd5138e7826121df92e3a693b9
Sharanjana Ragunathan <sharanjana10@gmail.com>
2026-08-04 16:27:16 +0530

    Add files via upload

---
+++ Day1/question.js
@@ -0,0 +1,29 @@
+1)swap 2 numbers without temporary variable
+
+2)find large numbers among given of numbers
+
+function findTopThree(arr){
+	let first = -Infinity,third = -Infinity;
+	
+	for (let num of arr){
+		if(num>first){
+			[third,second,first] = [second, first,num]
+			
+		}
+			
+	}
+}
+
+
+
+
+3)break a whole number into digits like 4562->4,5,6,2
+
+4)scale a matrix 
+
+5)multiply two matrix
+
+6)create a function to print  element from multi dimensional array
+     
+	 
+	 
